686057a152104b44e2805b17a559977e6f40fe48
TsegaeyesusGoha <tsegaeyesus.goha@gmail.com>
2025-08-09 17:02:04 +0300

    add Navbar

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router'
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+import { Navbar } from './components/Navbar.tsx'
 const client = new QueryClient()
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
     <QueryClientProvider client={client}>
-      <Router>
+      <Navbar/>
+   <Router>
     <App />
   </Router>
       </QueryClientProvider>
